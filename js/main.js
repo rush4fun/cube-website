2096e5cf8fe93cdac94336c055e05f13b62ee856
Сокольвак Євгеній <evgensokolvak2012@gmail.com>
2020-07-10 23:48:03 +0300

    add(optimize code): optimized code

--- js/main.js
+++ js/main.js
@@ -1,149 +1,11 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/index.js");
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ "./src/blocks/modules/footer/footer.js":
-/*!*********************************************!*\
-  !*** ./src/blocks/modules/footer/footer.js ***!
-  \*********************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-/***/ "./src/blocks/modules/header/header.js":
-/*!*********************************************!*\
-  !*** ./src/blocks/modules/header/header.js ***!
-  \*********************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
 var close = document.querySelector('.close');
 var menuCheckbox = document.querySelector('#menu-checkbox');
 close.addEventListener('click', function () {
   menuCheckbox.checked = false;
 });
-
-/***/ }),
-
-/***/ "./src/js/import/modules.js":
-/*!**********************************!*\
-  !*** ./src/js/import/modules.js ***!
-  \**********************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_header_header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! %modules%/header/header */ "./src/blocks/modules/header/header.js");
-/* harmony import */ var _modules_header_header__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_modules_header_header__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! %modules%/footer/footer */ "./src/blocks/modules/footer/footer.js");
-/* harmony import */ var _modules_footer_footer__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_footer_footer__WEBPACK_IMPORTED_MODULE_1__);
-
-
-
-/***/ }),
-
-/***/ "./src/js/index.js":
-/*!*************************!*\
-  !*** ./src/js/index.js ***!
-  \*************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _import_modules__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./import/modules */ "./src/js/import/modules.js");
-
 var cube = {
   portfolio: [{
-    id: 1,
+    id: 0,
     type: 'app',
     client: 'David lee1 - Architect',
     date: '2014',
@@ -153,7 +15,7 @@ var cube = {
     img: ['img/emma_back.jpg', 'img/emma_front.jpg'],
     like: true
   }, {
-    id: 2,
+    id: 1,
     type: 'web-design',
     client: 'David lee2 - Architect',
     date: '2014',
@@ -163,7 +25,7 @@ var cube = {
     img: ['img/john_back.jpg', 'img/john_front.jpg'],
     like: false
   }, {
-    id: 3,
+    id: 2,
     type: 'branding',
     client: 'David lee3 - Architect',
     date: '2014',
@@ -173,7 +35,7 @@ var cube = {
     img: [],
     like: false
   }, {
-    id: 4,
+    id: 3,
     type: 'branding',
     client: 'David lee4 - Architect',
     date: '2014',
@@ -183,7 +45,7 @@ var cube = {
     img: [],
     like: false
   }, {
-    id: 5,
+    id: 4,
     type: 'web-design',
     client: 'David lee5 - Architect',
     date: '2014',
@@ -193,7 +55,7 @@ var cube = {
     img: [],
     like: false
   }, {
-    id: 6,
+    id: 5,
     type: 'app',
     client: 'David lee6 - Architect',
     date: '2014',
@@ -203,7 +65,7 @@ var cube = {
     img: [],
     like: false
   }, {
-    id: 7,
+    id: 6,
     type: 'branding',
     client: 'David lee7 - Architect',
     date: '2014',
@@ -213,7 +75,7 @@ var cube = {
     img: [],
     like: false
   }, {
-    id: 8,
+    id: 7,
     type: 'app',
     client: 'David lee8 - Architect',
     date: '2014',
@@ -223,7 +85,7 @@ var cube = {
     img: [],
     like: false
   }, {
-    id: 9,
+    id: 8,
     type: 'app',
     client: 'David lee9 - Architect',
     date: '2014',
@@ -233,7 +95,7 @@ var cube = {
     img: [],
     like: false
   }, {
-    id: 10,
+    id: 9,
     type: 'app',
     client: 'David lee10 - Architect',
     date: '2010',
@@ -260,28 +122,22 @@ var cube = {
       portfolioLink.forEach(function (item) {
         item.addEventListener('click', function (event) {
           event.preventDefault();
-
           for (var i = 0; i < portfolioItem.length; i++) {
             portfolioItem[i].style.display = 'none';
-
             if (this.dataset.type == 'all') {
               portfolioItem[i].style.display = 'flex';
               var loadMoreBtn = document.querySelector('.js-loadMore');
               loadMoreBtn.style.display = 'none';
             }
-
             if (this.dataset.type == portfolioItem[i].dataset.type) {
               portfolioItem[i].style.display = 'flex';
-
               var _loadMoreBtn = document.querySelector('.js-loadMore');
-
               _loadMoreBtn.style.display = 'none';
             }
           }
         });
       });
     };
-
     portfolioNavigation();
   },
   loadMore: function loadMore() {
@@ -297,7 +153,7 @@ var cube = {
   },
   openPortfolioCase: {
     popUp: document.querySelector('#popup'),
-    loadPopUpContent: function loadPopUpContent(curId) {
+    loadPopUpContent: function (curId) {
       for (var i = 0; i < cube.portfolio.length; i++) {
         if (cube.portfolio[i].id == curId) {
           var loadPicturePopUp = function loadPicturePopUp(index) {
@@ -305,23 +161,18 @@ var cube = {
               var picturePopUp = document.querySelector('.js-picture');
               picturePopUp.src = cube.portfolio[index].img[currStep];
             };
-
             var step = 0;
             loadPicture(step);
-
             var countPictures = function countPictures() {
               var currentPicIndex = document.querySelector('.js-popUpCurrentPicIndex');
               var picLenght = document.querySelector('.js-popUpPicLenght');
-
               if (cube.portfolio[index].img.length != 0) {
                 currentPicIndex.innerHTML = step + 1;
               } else {
                 currentPicIndex.innerHTML = step;
               }
-
               picLenght.innerHTML = cube.portfolio[index].img.length;
             };
-
             countPictures();
             var prevPic = document.querySelector('.js-popUpPrevPic');
             var nextPic = document.querySelector('.js-popUpNextPic');
@@ -330,9 +181,7 @@ var cube = {
                 step++;
                 loadPicture(step);
                 countPictures();
-              }
-
-              ;
+              };
             });
             prevPic.addEventListener('click', function () {
               if (step > 0) {
@@ -342,44 +191,36 @@ var cube = {
               }
             });
           };
-
           var loadClientPopUp = function loadClientPopUp(index) {
             var clientPopUp = document.querySelector('.js-popUpCleint');
             clientPopUp.lastChild.textContent = cube.portfolio[index].client;
           };
-
           var loadDatePopUp = function loadDatePopUp(index) {
             var datePopUp = document.querySelector('.js-popUpDate');
             datePopUp.lastChild.textContent = cube.portfolio[index].date;
           };
-
           var loadSkillsPopUp = function loadSkillsPopUp(index) {
             var skillsPopUp = document.querySelector('.js-popUpSkills');
             skillsPopUp.lastChild.textContent = cube.portfolio[index].skills;
           };
-
           var loadTextPopUp = function loadTextPopUp(index) {
             var textPopUp = document.querySelector('.js-popUpText');
             textPopUp.innerHTML = cube.portfolio[index].text;
           };
-
           var loadSitePopUp = function loadSitePopUp(index) {
             var sitePopUp = document.querySelector('.js-popUpSite');
             sitePopUp.lastChild.textContent = cube.portfolio[index].site;
           };
-
-          var loadLikes = function loadLikes(index) {
+          var loadLikes = function (index) {
             var popUpLike = document.querySelector('.js-like');
-
             if (cube.portfolio[index].like == false) {
               popUpLike.classList.remove('like--red');
             } else if (cube.portfolio[index].like == true) {
               popUpLike.classList.add('like--red');
             }
-
-            popUpLike.addEventListener('click', function () {
-              var currId = cube.openPortfolioCase.popUp.dataset.id - 1;
-
+            popUpLike.addEventListener('click', function (event) {
+              event.preventDefault();
+              var currId = cube.openPortfolioCase.popUp.dataset.id;
               if (cube.portfolio[currId].like == false) {
                 popUpLike.classList.add('like--red');
                 cube.portfolio[index].like = true;
@@ -389,7 +230,6 @@ var cube = {
               }
             });
           };
-
           loadSitePopUp(i);
           loadTextPopUp(i);
           loadSkillsPopUp(i);
@@ -400,13 +240,14 @@ var cube = {
         }
       }
     },
-    openPopUp: function openPopUp() {
+    openPopUp: function () {
       var portfolioItem = document.querySelectorAll('.js-openPopUp');
       portfolioItem.forEach(function (item) {
         item.addEventListener('click', function (event) {
           event.preventDefault();
           cube.openPortfolioCase.popUp.style.display = 'block';
           var currentId = this.closest('.portfolio__item').dataset.id;
+          console.log(currentId);
           cube.openPortfolioCase.popUp.dataset.id = currentId;
           cube.openPortfolioCase.loadPopUpContent(currentId);
         });
@@ -423,27 +264,21 @@ var cube = {
       nextProject.addEventListener('click', function (event) {
         event.preventDefault();
         var popUpCurrentId = cube.openPortfolioCase.popUp.dataset.id;
-
         if (popUpCurrentId != cube.portfolio[cube.portfolio.length - 1].id) {
           var nextPopUpId = ++popUpCurrentId;
           cube.openPortfolioCase.popUp.dataset.id = nextPopUpId;
           cube.openPortfolioCase.loadPopUpContent(nextPopUpId);
-        }
-
-        ;
+        };
       });
       var prevProject = document.querySelector('.js-popUpPrevProject');
       prevProject.addEventListener('click', function (event) {
         event.preventDefault();
         var popUpCurrentId = cube.openPortfolioCase.popUp.dataset.id;
-
-        if (popUpCurrentId > 1) {
+        if (popUpCurrentId > 0) {
           var prevPopUpId = --popUpCurrentId;
           cube.openPortfolioCase.popUp.dataset.id = prevPopUpId;
           cube.openPortfolioCase.loadPopUpContent(prevPopUpId);
-        }
-
-        ;
+        };
       });
     },
     init: function init() {
@@ -462,8 +297,3 @@ var cube = {
 document.addEventListener('DOMContentLoaded', function () {
   cube.init();
 });
-
-/***/ })
-
-/******/ });
-//# sourceMappingURL=main.js.map
